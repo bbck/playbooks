79e31b7b46fb3483f52cb7bcfd77efacf0b48e32
Chris Boyle <chris@bbck.net>
2017-04-02 14:53:25 -0400

    Update duplicity script

--- roles/plex/files/duplicity.sh
+++ roles/plex/files/duplicity.sh
@@ -6,4 +6,15 @@ export PASSPHRASE
 export AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY
 
-duplicity --s3-use-ia --encrypt-key 8B8E5ECC incr /var/lib/plexmediaserver "s3+http://bbck-plex/"
+GPG_KEY="8B8E5ECC"
+
+duplicity \
+  --s3-use-ia \
+  --asynchronous-upload \
+  --encrypt-key="$GPG_KEY" \
+  /var/lib/plexmediaserver \
+  "s3+http://bbck-plex/"
+
+unset PASSPHRASE
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
